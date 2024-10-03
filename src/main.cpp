e008adfa24a25a9d09e5e952eb4fc3df152bf95d
dormon <imilet@fit.vutbr.cz>
2024-10-03 13:20:28 +0200

    added sdl

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,18 @@
+#include<SDL.h>
+
 int main(int argc,char*argv[]){
+  auto window = SDL_CreateWindow("PGR2024",0,0,1024,768,0);
+
+  bool running = true;
+  while(running){
+    SDL_Event event;
+    while(SDL_PollEvent(&event)){
+      if(event.type == SDL_QUIT)
+        running = false;
+    }
+  }
+
+  SDL_DestroyWindow(window);
   return 0;
+
 }
