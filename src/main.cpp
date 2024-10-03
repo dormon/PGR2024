f7111266a9bd627a7d42c7577b613c48bc204095
dormon <imilet@fit.vutbr.cz>
2024-10-03 13:26:56 +0200

    added context

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include<SDL.h>
 
 int main(int argc,char*argv[]){
-  auto window = SDL_CreateWindow("PGR2024",0,0,1024,768,0);
+  auto window = SDL_CreateWindow("PGR2024",0,0,1024,768,SDL_WINDOW_OPENGL);
+  auto context = SDL_GL_CreateContext(window);
 
   bool running = true;
   while(running){
@@ -10,8 +11,11 @@ int main(int argc,char*argv[]){
       if(event.type == SDL_QUIT)
         running = false;
     }
+
+    SDL_GL_SwapWindow(window);
   }
 
+  SDL_GL_DeleteContext(context);
   SDL_DestroyWindow(window);
   return 0;
 
