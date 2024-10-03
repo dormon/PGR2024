9d04692b5010a061209cfe44e102d5733ad6f0ce
dormon <imilet@fit.vutbr.cz>
2024-10-03 13:37:05 +0200

    opengl functions

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,14 @@ int main(int argc,char*argv[]){
   auto window = SDL_CreateWindow("PGR2024",0,0,1024,768,SDL_WINDOW_OPENGL);
   auto context = SDL_GL_CreateContext(window);
 
+
+  using GLCLEARCOLOR = void(*)(float,float,float,float);
+  using GLCLEAR      = void(*)(uint32_t);
+#define GL_COLOR_BUFFER_BIT			0x00004000
+
+  GLCLEARCOLOR glClearColor = (GLCLEARCOLOR)SDL_GL_GetProcAddress("glClearColor");
+  GLCLEAR      glClear      = (GLCLEAR     )SDL_GL_GetProcAddress("glClear"     );
+
   bool running = true;
   while(running){
     SDL_Event event;
@@ -12,6 +20,8 @@ int main(int argc,char*argv[]){
         running = false;
     }
 
+    glClearColor(0,0,1,1);
+    glClear(GL_COLOR_BUFFER_BIT);
     SDL_GL_SwapWindow(window);
   }
 
