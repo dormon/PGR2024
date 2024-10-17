e62875dabdd2cc35db0ef6713f1e4d3e057f8e80
dormon <imilet@fit.vutbr.cz>
2024-10-17 11:54:00 +0200

    geGL prg

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include "geGL/Program.h"
+#include "geGL/Shader.h"
 #include "glm/ext/matrix_transform.hpp"
 #include "glm/gtc/constants.hpp"
 #include<SDL.h>
@@ -10,36 +12,6 @@
 #include<timer.hpp>
 
 using namespace ge::gl;
-
-std::string shaderToStr(GLenum type){
-  if(type == GL_VERTEX_SHADER         )return "GL_VERTEX_SHADER"         ;
-  if(type == GL_FRAGMENT_SHADER       )return "GL_FRAGMENT_SHADER"       ;
-  if(type == GL_GEOMETRY_SHADER       )return "GL_GEOMETRY_SHADER"       ;
-  if(type == GL_TESS_CONTROL_SHADER   )return "GL_TESS_CONTROL_SHADER"   ;
-  if(type == GL_TESS_EVALUATION_SHADER)return "GL_TESS_EVALUATION_SHADER";
-  if(type == GL_COMPUTE_SHADER        )return "GL_COMPUTE_SHADER"        ;
-  return "";
-}
-
-GLuint createShader(GLenum type,std::string const&src){
-  char const*srcs[]={
-    src.c_str()
-  };
-  GLuint s = glCreateShader(type);
-  glShaderSource(s,1,srcs,nullptr);
-  glCompileShader(s);
-
-  GLint status;
-  glGetShaderiv(s,GL_COMPILE_STATUS,&status);
-  if(status != GL_TRUE){
-    char buffer[1024] = {0};
-    glGetShaderInfoLog(s,1024,nullptr,buffer);
-    std::cerr << "ERROR: " << shaderToStr(type) << " shader compilation failed" << std::endl;
-    std::cerr << std::string(buffer) << std::endl;
-  }
-
-  return s;
-}
 
 int main(int argc,char*argv[]){
   int width  = 1024;
@@ -75,7 +47,6 @@ int main(int argc,char*argv[]){
     if(gl_VertexID == 2){vColor = vec3(0,0,1);gl_Position = mvp*vec4(0,1,0,1);}
   }
   ).";
-  auto vs = createShader(GL_VERTEX_SHADER,vsSrc);
 
   char const*fsSrc = R".(
   #version 430
@@ -88,13 +59,9 @@ int main(int argc,char*argv[]){
   }
   ).";
 
-  auto fs = createShader(GL_FRAGMENT_SHADER,fsSrc);
-
-
-  GLuint prg = glCreateProgram();
-  glAttachShader(prg,vs);
-  glAttachShader(prg,fs);
-  glLinkProgram(prg);
+  auto vs = std::make_shared<Shader>(GL_VERTEX_SHADER  ,vsSrc);
+  auto fs = std::make_shared<Shader>(GL_FRAGMENT_SHADER,fsSrc);
+  auto prg = std::make_shared<Program>(vs,fs);
 
   GLuint vao;
   glCreateVertexArrays(1,&vao);
@@ -102,13 +69,6 @@ int main(int argc,char*argv[]){
   glEnable(GL_DEPTH_TEST);
 
   auto timer = Timer<float>();
-
-  GLuint iTimeLocation          = glGetUniformLocation(prg,"iTime"         );
-  GLuint cameraYAngleLocation   = glGetUniformLocation(prg,"cameraYAngle"  );
-  GLuint cameraXAngleLocation   = glGetUniformLocation(prg,"cameraXAngle"  );
-  GLuint cameraDistanceLocation = glGetUniformLocation(prg,"cameraDistance");
-  GLuint projLocation           = glGetUniformLocation(prg,"proj"          );
-  GLuint viewLocation           = glGetUniformLocation(prg,"view"          );
 
   float cameraYAngle   = 0.f;
   float cameraXAngle   = 0.f;
@@ -144,25 +104,21 @@ int main(int argc,char*argv[]){
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 
     glBindVertexArray(vao);
-    glUseProgram(prg);
-
-    glProgramUniform1f(prg,iTimeLocation,timer.elapsedFromStart());
-    glProgramUniform1f(prg,cameraYAngleLocation  ,cameraYAngle  );
-    glProgramUniform1f(prg,cameraXAngleLocation  ,cameraXAngle  );
-    glProgramUniform1f(prg,cameraDistanceLocation,cameraDistance);
 
     float aspect = (float)width / (float)height;
     auto proj = glm::perspective(glm::half_pi<float>(),aspect,0.1f,1000.f);
-    glProgramUniformMatrix4fv(prg,projLocation,1,GL_FALSE,(float*)&proj);
 
     auto CT = glm::translate(glm::mat4(1.f),glm::vec3(0,0,-cameraDistance));
     auto CRX = glm::rotate(glm::mat4(1.f),cameraXAngle,glm::vec3(1.f,0.f,0.f));
     auto CRY = glm::rotate(glm::mat4(1.f),cameraYAngle,glm::vec3(0.f,1.f,0.f));
     auto view = CT * CRX * CRY;
-    glProgramUniformMatrix4fv(prg,viewLocation,1,GL_FALSE,(float*)&view);
+
+    prg->use();
+    prg->set1f       ("iTime",timer.elapsedFromStart());
+    prg->setMatrix4fv("proj" ,(float*)&proj);
+    prg->setMatrix4fv("view" ,(float*)&view);
 
     glDrawArrays(GL_TRIANGLES,0,3);
-
 
     SDL_GL_SwapWindow(window);
   }
